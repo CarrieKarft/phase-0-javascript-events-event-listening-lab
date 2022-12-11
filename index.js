82cf2207d9926a229cefbac3c0449a296d6a5b5b
Carrie Kraft <carriekarft@gmail.com>
2022-12-11 13:18:41 -0800

    commit tree df9b4ff574edb8d8fe797c20636c77c8760eca9c

--- index.js
+++ index.js
@@ -1,2 +1,8 @@
+// const button = document.getElementById('buttton');
 function addingEventListener() {
+    const button = document.getElementById('button');
+    button.addEventListener('click', function() {
+        alert('I was clicked!');
+      });
 }
+addingEventListener()
